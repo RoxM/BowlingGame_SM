ae97f034fc13ed0d3fadc3d09f7c59383c76a94f
Rox <rossymr@gmail.com>
2015-10-12 15:58:26 -0500

    RED All ones

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,4 +8,10 @@ it "returns 0 for gutter game" do
     expect(@bowling.score).to eq(0)
 end
 
+it "returns 20 for all ones" do
+	@bowling = BowlingGame.new
+    20.times {@bowling.roll(1)}
+    expect(@bowling.score).to eq(20)
+end
+
 end
