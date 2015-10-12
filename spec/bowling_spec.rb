f509f44ef4c82ee4c5a35e42334eb7aae0b50c7f
Rox <rossymr@gmail.com>
2015-10-12 15:54:56 -0500

    RED - Gutter game

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,5 +2,10 @@ require_relative '../bowling'
 
 describe BowlingGame do
 
+it "returns 0 for gutter game" do
+	@bowling = BowlingGame.new
+    20.times {@bowling.roll(0)}
+    expect(@bowling.score).to eq(0)
+end
 
 end
