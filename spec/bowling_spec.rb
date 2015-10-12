061e0688b0e7442751c4102c5d07a39b3fcf4602
Rox <rossymr@gmail.com>
2015-10-12 16:02:57 -0500

    REFACT all ones
    Only on tests

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,14 +2,16 @@ require_relative '../bowling'
 
 describe BowlingGame do
 
-it "returns 0 for gutter game" do
+before do
 	@bowling = BowlingGame.new
+end
+
+it "returns 0 for gutter game" do
     20.times {@bowling.roll(0)}
     expect(@bowling.score).to eq(0)
 end
 
 it "returns 20 for all ones" do
-	@bowling = BowlingGame.new
     20.times {@bowling.roll(1)}
     expect(@bowling.score).to eq(20)
 end
